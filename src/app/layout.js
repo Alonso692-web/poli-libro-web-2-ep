953d88e06255f3954888fe8b359c6d7be2ae5dac
Alonso <alonsolopezdominguez3@gmail.com>
2025-07-11 11:00:34 -0600

    V5.4.1 Add Analytics
    Se agregó el componente Analytics para ver las esatdísticas de visitas de la página

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           <Header />
           <main className="flex-grow">
             {children}
+            <Analytics />
           </main>
           <Footer />
         </div>
